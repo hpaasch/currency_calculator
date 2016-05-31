250e904ed21e4ab75eee7a2d4e3981e282912c03
hpaasch <hepaasch@gmail.com>
2016-05-31 16:32:36 -0400

    Normal Mode completed.

--- my_calculator.py
+++ my_calculator.py
@@ -2,10 +2,6 @@
 # Implement >= > <= < + - == != * operator overloads.
 
 # example >>> Money(100.00, "USD") + Money(56.32, "EUR")
-# output in USD
-
-# currency_list = ["USD", "JPY", "EUR", "BTC"]
-
 
 class Money:
 
@@ -13,7 +9,6 @@ class Money:
         # could seek input for the amount and currency
         self.amount = amount
         self.currency = currency
-        # self.wallet = amount * currency  # this will be the amount * us currency equivalent
 
     def __str__(self):
         return "{} {}".format(self.amount, self.currency)
@@ -30,30 +25,53 @@ class Money:
     def __le__(self, other):
         return self.wallet_value() <= other.wallet_value()
 
+    def __add__(self, other):
+        return self.wallet_value() + other.wallet_value()
+
+    def __sub__(self, other):
+        return self.wallet_value() + other.wallet_value()
+
+    def __eq__(self, other):
+        return self.wallet_value() + other.wallet_value()
+
+    def __ne__(self, other):
+        return self.wallet_value() + other.wallet_value()
+
+    def __mul__(self, other):
+        return self.wallet_value() + other.wallet_value()
+
     def wallet_value(self):
         if self.currency == "USD":
-            self.amount *= 1
+            return self.amount * 1
         elif self.currency == "JPY":
-            self.amount *= 110.762
+            return self.amount * 110.762
         elif self.currency == "EUR":
-            self.amount *= 1.11363
+            return self.amount * 1.11363
         elif self.currency == "BTC":
-            self.amount *= 534.015
-        return self.amount
+            return self.amount * 534.015
 
 
-
-
-rayn_money = Money(200, "JPY")
-hep_money = Money(100, "USD")
-print(hep_money)
-print(rayn_money)
-print(hep_money < rayn_money)
-print(hep_money > rayn_money)
-print(hep_money >= rayn_money)
-print(hep_money <= rayn_money)
-
-
-
-
-
+rayn_money = Money(1, "JPY")
+hep_money = Money(50, "USD")
+davis_money = Money(25, "BTC")
+eileen_money = Money(30, "EUR")
+print("HEP put in {} {}. It's worth ${} American.".format(hep_money.amount, hep_money.currency,
+                                                    hep_money.wallet_value()))
+print("Rayn put in {} {}. It's worth ${} American.".format(rayn_money.amount, rayn_money.currency,
+                                                    rayn_money.wallet_value()))
+print("Davis put in {} {}. It's worth ${} American.".format(davis_money.amount, davis_money.currency,
+                                                          davis_money.wallet_value()))
+print("Eileen put in {} {}. It's worth ${} American.".format(eileen_money.amount, eileen_money.currency,
+                                                            eileen_money.wallet_value()))
+print("HEP less than Rayn?", hep_money < rayn_money)  # true
+print("HEP greater than Davis?", hep_money > davis_money)  # false
+print("HEP greater than or equal to Eileen?", hep_money >= eileen_money)  # true
+print("HEP less than or equal to Eileen?", hep_money <= eileen_money)  # false
+print("HEP added to Rayn: ${} American.".format(hep_money.wallet_value() +
+                                                rayn_money.wallet_value()))
+print("HEP subtracted from Davis: ${} American.".format((davis_money.wallet_value() -
+                                                         hep_money.wallet_value())))
+print("Davis equal to Rayn?", davis_money.wallet_value() == rayn_money.wallet_value())
+print("Eileen not equal to Davis?", eileen_money.wallet_value() != davis_money.wallet_value())
+print("Rayn multiplied by Eileen: ${} American.".format(rayn_money.wallet_value() *
+                                                        eileen_money.wallet_value()))
